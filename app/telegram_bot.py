c5144725530cd6797b37b5528c18b5d84595b2f9
Youliya Bratishko <youliyav@gmail.com>
2024-03-18 19:06:25 +0300

    Add keyboard

--- app/telegram_bot.py
+++ app/telegram_bot.py
@@ -1,29 +1,51 @@
-from telegram.ext import Application, CommandHandler
 import logging
+
 from telegram import Update
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+
 from task_service import service
 from config import config
-
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-async def start(update, context):
-    await update.message.reply_text('Hello! I can fetch Jira issues for you.')
+keyboard_buttons = [
+    [InlineKeyboardButton('Fetch Jira Issues', callback_data='fetch_issues')],
+    [InlineKeyboardButton('Help', callback_data='help_command')],
+]
 
-async def get_issues(update, context):
+keyboard = InlineKeyboardMarkup(keyboard_buttons)
+
+async def handle_keyboard(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Choose an action:', reply_markup=keyboard)  # type: ignore
+
+async def fetch_issues(update: Update, context):
     total_issues = service.get_total_issues_jira()
-    await update.message.reply_text(f"Total issues: {total_issues}")
+    await update.callback_query.edit_message_text(f"Total issues: {total_issues}", reply_markup=keyboard)  # type: ignore
+    await update.callback_query.answer('Issues fetched successfully.')  # type: ignore
+
+async def help_command(update: Update, context):
+    await update.callback_query.edit_message_text(f"Use `/start` to fetch Jira issues.", reply_markup=keyboard)  # type: ignore
+    await update.callback_query.answer('Help command successfully.')  # type: ignore
+
+
 
 def start_bot():
     app = Application.builder().token(config.TELEGRAM_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("getissues", get_issues))
-    app.run_polling()
+
+    app.add_handler(CommandHandler('start', handle_keyboard))
+    app.add_handler(CallbackQueryHandler(fetch_issues, pattern='fetch_issues'))
+    app.add_handler(CallbackQueryHandler(help_command, pattern='help_command'))
+
+    try:
+        app.run_polling()
+    except Exception as e:
+        logging.error(f'An error occurred: {e}')
+    else:
+        logging.info('Bot stopped.')
 
 if __name__ == '__main__':
     start_bot()
-
-
