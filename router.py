c76903f16ec3dd73ccd720b8730b6bc271670197
Youliya Bratishko <youliyav@gmail.com>
2024-05-03 16:52:08 +0300

    Add def lifespan()

---
+++ router.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi import APIRouter
+from repository import TaskRepository
+from schemas import STask, STaskAdd, STaskId
+
+
+router = APIRouter(
+    prefix="/tasks",
+    tags=["tasks"],
+)
+
+@router.post("")
+async def add_task(task: Annotated[STaskAdd, Depends()],) -> STaskId:
+    task_id = await TaskRepository.add_task(task)
+    return {"ok": True, "task_id": task_id}  # type: ignore
+
+@router.get("")
+async def get_tasks() -> list[STask]:
+    tasks = await TaskRepository.get_tasks()
+    return tasks
